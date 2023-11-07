6c0032f5cc2b842e234bd098a58b1019f33ce72d
NataDeNata <124381307+NataDeNata@users.noreply.github.com>
2023-11-07 16:41:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// JavaScript to show/hide the pop-up image
-document.getElementById("showImageBtn").addEventListener("click", function() {
-    document.getElementById("imagePopup").style.display = "block";
-});
+function updateClock() {
+    const now = new Date();
+    const timeElement = document.getElementById('time');
+    const dateElement = document.getElementById('date');
 
-document.getElementById("closeBtn").addEventListener("click", function() {
-    document.getElementById("imagePopup").style.display = "none";
-});
+    const options = { year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const dateTimeString = now.toLocaleDateString(undefined, options);
+
+    timeElement.textContent = dateTimeString;
+
+    setTimeout(updateClock, 1000); // Update every second
+}
+
+updateClock();
